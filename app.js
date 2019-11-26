c52337b3f8e3e21de9326670c659247ae3ff79c9
Moshe Avni <moshe.avni@naturalint.com>
2019-11-26 13:53:22 +0200

    Remove gitignore and add favicon

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require("body-parser");
 const express = require('express');
+const favicon = require("serve-favicon");
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -20,6 +21,8 @@ app.use(bodyParser.json());
 // Logger
 app.use(morgan('dev'));
 
+// Favicon
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 app.get('*', (req, res) => {
     return res.render('index', {
