20594c6f6de2423cb3292388bafa29fc4837773b
Moshe Avni <mavni@netapp.com>
2022-12-11 14:56:39 +0200

    fix repo name

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-if [[ -n $1 ]];then
+if [[ -n $1 ]]; then
   new_version=$1
 else
   new_version=$(jq -r '.version | split(".") | first | tonumber + 1' package.json)
 fi
-jq '.version="'$new_version'.0.0"' package.json > package.json.new
-docker build -t naturalintmma/express:${new_version} .
-docker tag naturalintmma/express:${new_version} naturalintmma/express:latest
-docker push naturalintmma/express:${new_version}
-docker push naturalintmma/express:latest
+jq '.version="'$new_version'.0.0"' package.json >package.json.new
+REPO=mosheavni/express
+docker build -t ${REPO}:${new_version} .
+docker tag ${REPO}:${new_version} ${REPO}:latest
+docker push ${REPO}:${new_version}
+docker push ${REPO}:latest
 
 rm -f package.json
 mv package.json.new package.json
